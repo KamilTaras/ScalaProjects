382a170bab844f9a575bce7e68d68e564669a5d1
KamilTaras <kamiltaras0@gmail.com>
2022-11-23 18:50:28 +0100

    updating Anonymous Function Class

--- src/main/scala/lectures/part3Functional/AnonymousFunctions.scala
+++ src/main/scala/lectures/part3Functional/AnonymousFunctions.scala
@@ -1,34 +1,60 @@
 package lectures.part3Functional
 
 object AnonymousFunctions extends App{
+  val doubler = new Function1[Int, Int] {
+    override def apply(x: Int) = x * 2
+  }
 
-//  anonymous function
-  val doubler: Int => Int = x => x*2
-//  multiple parameters
-  val adder: (Int, Int) => Int = (a,b) => a + b
-//  no parameter
+  //anonymous function (LAMBDA)
+  val sugarDoubler: Int => Int = x => x * 2
+
+  //multiple params in a lambda
+
+  val adder: (Int, Int) => Int = (a: Int, b: Int) => a + b
+
+  //no params
   val justDoSth: () => Int = () => 3
 
 
-//be careful
-  println(justDoSth)
-  println(justDoSth())
+  //CAREFUL
+  println(justDoSth) //functions itself
+  println(justDoSth()) //call
 
-//  curly braces with LAMBDA
 
+  //curly braces with lambdas
   val stringToInt = { (str: String) =>
     str.toInt
   }
 
+  //MOAR syntactic sugar
+  val niceIncrementer: Int => Int = _ + 1 //equivalent to x => x + 1
+  val niceAdder: (Int, Int) => Int = _ + _
 
-//  MOAR syntactic sugar
-  val niceIncrementer:Int => Int = x => x+1
-  val nicerIncrementer: Int => Int = _ + 1
+  /*
 
-  val niceAdder: (Int, Int) => Int = (a,b) => a+b
-  val nicerAdder: (Int, Int) =>Int = _+_
+  TASKS:
+
+  Expand MyList
+  -foreach method A => Unit
+  [1,2,3].foreach (x=> println(x))
+
+  -sort function ((A,A) => Int) => MyList
+    [1,2,3].sort((x,y) => y-x) => [3,2,1]
+
+  -zipWith (list, (A,A)
+    [1,2,3].zipWith([4,5,6], x*y ) => [1*4, 2*5, 3*6] = [4,10,18]
+
+  -fold(start)(function) => a value
+    [1,2,3].fold(0)(x+y) = y
 
 
-  val supperAdd = (x: Int) => (y:Int) => x+y
+2. Define a method, curried & uncurried version)
 
+  toCurry(f: (Int, Int) => Int)  => (Int => Int => Int)
+  fromCurry(f:(Int => Int => Int)) => (Int, Int) => Int
+
+3. compose (f,g) => x => f(g(x))
+   andThen(f,g) => x => g(f(x))
+
+    */
 }
